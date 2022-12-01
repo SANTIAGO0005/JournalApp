e8029e647b648c2ded5042c696244887fb81cd8c
Santiago Cifuentes <cifuentess938@gmail.com>
2022-11-30 22:05:52 -0500

    starting configurations for firestore and deploying crud

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,35 @@
 import { AddOutlined } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { savingNewNote } from "../../store/journal/journalSlice";
+import { startNewNote } from "../../store/journal/thunks";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
+
+  const { isSaving, active} = useSelector( state => state.journal)
+  const dispatch = useDispatch()
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote())
+    
+  }
   return (
     <JournalLayout>
-      <NothingSelectedView />
 
-      {/* <NoteView/> */}
+      { 
+      (!!active)
+      ? <NoteView/>
+      :<NothingSelectedView />
+      }
+      
+
+      
 
       <IconButton
+        disabled={ isSaving}
+        onClick={ onClickNewNote }
         size="large"
         sx={{
           color: "white",
